8c157f2bd7324edf868d8cba56e5d131c0adef52
MaylaBeatriz <maylaetb@gmail.com>
2022-01-03 11:23:54 -0300

    atribuindo a url para o service

--- bytebank/src/app/services/transferencia.service.ts
+++ bytebank/src/app/services/transferencia.service.ts
@@ -7,6 +7,7 @@ import { Injectable } from '@angular/core';
 export class TransferenciaService {
 
   private listaTransferencia: any[];
+  private url = 'http://localhost:3000/transferencias';
 
   constructor(private httpClient: HttpClient) {
     this.listaTransferencia = [];
